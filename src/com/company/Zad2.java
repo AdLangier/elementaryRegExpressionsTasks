45b749a9ba30f6f435327df8246ccea76eef3243
Adrian Langier <langier.adrian@gmail.com>
2021-03-19 21:45:17 +0100

    solved Task3,4,5

--- src/com/company/Zad2.java
+++ src/com/company/Zad2.java
@@ -7,7 +7,7 @@ public class Zad2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj kod pocztowy");
-        String zipCode = scanner.next();
+        String zipCode = scanner.nextLine();
 
         Pattern pattern = Pattern.compile("[0-9]{2}-[0-9]{3}");
         if (pattern.matcher(zipCode).find()) {                          //Jeśli matcher zwraca true - kod poprawny, jesli nie to niepoprawny
---
+++ src/com/company/Zad3.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Zad3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj wyraz");
+        String word = scanner.next();
+
+        Pattern pattern = Pattern.compile("ab+");
+        if (pattern.matcher(word).find()){
+            System.out.println("sekwencja \"ab\" występuje w podanym wyrazie");
+        } else {
+            System.out.println("sekwencja \"ab\" nie występuje w podanym wyrazie");
+        }
+    }
+}
