d2febb054426e4bf651d08f333a2171f24fde399
Adrian Langier <langier.adrian@gmail.com>
2021-03-20 18:12:35 +0100

    review Task2

--- src/reviewTasks/Zad2.java
+++ src/reviewTasks/Zad2.java
@@ -1,7 +1,20 @@
 package reviewTasks;
 
+import java.util.Scanner;
+
 public class Zad2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj adres");
+        String url = scanner.next();
+
+        if (url.startsWith("http://")
+                || url.startsWith("https://")
+                || url.startsWith("www.")) {
+            System.out.println("To jest poprawny adres URL");
+        } else {
+            System.out.println("Adres jest nie poprawny");
+        }
 
     }
 }
